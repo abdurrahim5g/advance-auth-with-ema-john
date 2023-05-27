f1435630a663fc118a54cb6da96af3ea090bb815
f-rahim <freelancerrahim26@gmail.com>
2023-05-27 12:09:56 +0600

    Config firebase & install react-router

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4ory_pfT1pXFNvgubxGjCjXlKX4JF8oE",
+  authDomain: "advance-auth-with-ema-john.firebaseapp.com",
+  projectId: "advance-auth-with-ema-john",
+  storageBucket: "advance-auth-with-ema-john.appspot.com",
+  messagingSenderId: "550825554348",
+  appId: "1:550825554348:web:1315cd53725256d92a0a09"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
