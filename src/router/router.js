8643754ed896dc59bcaa0618a1699919d0d03b8a
f-rahim <freelancerrahim26@gmail.com>
2023-05-27 12:24:15 +0600

    Setup login & register router

---
+++ src/router/router.js
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Shop from "../components/Shop/Shop";
+import Orders from "../components/Orders/Orders";
+import Inventory from "../components/Inventory/Inventory";
+import About from "../components/About/About";
+import { productsAndCartLoader } from "../loaders/productsAndCartLoader";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        loader: () => fetch("products.json"),
+        element: <Shop></Shop>,
+      },
+      {
+        path: "orders",
+        loader: productsAndCartLoader,
+        element: <Orders></Orders>,
+      },
+      {
+        path: "inventory",
+        element: <Inventory></Inventory>,
+      },
+      {
+        path: "about",
+        element: <About></About>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+export default router;
